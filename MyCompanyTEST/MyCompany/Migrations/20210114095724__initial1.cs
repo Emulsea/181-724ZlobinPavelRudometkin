0a90b6dad3b8424044fc568d6a610e653a340e04
Emulsea <63652824+Emulsea@users.noreply.github.com>
2021-01-14 16:48:24 +0300

    finish
    finish

---
+++ MyCompanyTEST/MyCompany/Migrations/20210114095724__initial1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyCompany.Migrations
+{
+    public partial class _initial1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "44546e06-8719-4ad8-b88a-f271ae9d6eab",
+                column: "ConcurrencyStamp",
+                value: "e6b8d6a9-f7e4-4fcd-9bb7-538a8d6e0f0e");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "3b62472e-4f66-49fa-a20f-e7685b9565d8",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash" },
+                values: new object[] { "51943a27-42ba-49c4-a474-c53736419da2", "AQAAAAEAACcQAAAAELo2GXQ3M9ir+ZxwucuXMweexPoZvsdR012Z6RhYUmfl1Ji6a9eBFSwHGxafTB0QPA==" });
+
+            migrationBuilder.UpdateData(
+                table: "TextFields",
+                keyColumn: "Id",
+                keyValue: new Guid("4aa76a4c-c59d-409a-84c1-06e6487a137a"),
+                column: "DateAdded",
+                value: new DateTime(2021, 1, 14, 9, 57, 24, 500, DateTimeKind.Utc).AddTicks(3285));
+
+            migrationBuilder.UpdateData(
+                table: "TextFields",
+                keyColumn: "Id",
+                keyValue: new Guid("63dc8fa6-07ae-4391-8916-e057f71239ce"),
+                column: "DateAdded",
+                value: new DateTime(2021, 1, 14, 9, 57, 24, 500, DateTimeKind.Utc).AddTicks(890));
+
+            migrationBuilder.UpdateData(
+                table: "TextFields",
+                keyColumn: "Id",
+                keyValue: new Guid("70bf165a-700a-4156-91c0-e83fce0a277f"),
+                column: "DateAdded",
+                value: new DateTime(2021, 1, 14, 9, 57, 24, 500, DateTimeKind.Utc).AddTicks(3225));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "44546e06-8719-4ad8-b88a-f271ae9d6eab",
+                column: "ConcurrencyStamp",
+                value: "1c07fb23-ced7-48f9-bf0a-e0df233cd7a3");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "3b62472e-4f66-49fa-a20f-e7685b9565d8",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash" },
+                values: new object[] { "091be5bf-9d58-40e6-98a8-e82859894571", "AQAAAAEAACcQAAAAECBA6mt3xGNgyLjwyRhhtI3PI2IBKsm00Y3bAJfjdVrgT0++e45OV5Vh4SLTLPDHEQ==" });
+
+            migrationBuilder.UpdateData(
+                table: "TextFields",
+                keyColumn: "Id",
+                keyValue: new Guid("4aa76a4c-c59d-409a-84c1-06e6487a137a"),
+                column: "DateAdded",
+                value: new DateTime(2020, 2, 8, 6, 13, 50, 97, DateTimeKind.Utc).AddTicks(2284));
+
+            migrationBuilder.UpdateData(
+                table: "TextFields",
+                keyColumn: "Id",
+                keyValue: new Guid("63dc8fa6-07ae-4391-8916-e057f71239ce"),
+                column: "DateAdded",
+                value: new DateTime(2020, 2, 8, 6, 13, 50, 96, DateTimeKind.Utc).AddTicks(9537));
+
+            migrationBuilder.UpdateData(
+                table: "TextFields",
+                keyColumn: "Id",
+                keyValue: new Guid("70bf165a-700a-4156-91c0-e83fce0a277f"),
+                column: "DateAdded",
+                value: new DateTime(2020, 2, 8, 6, 13, 50, 97, DateTimeKind.Utc).AddTicks(2218));
+        }
+    }
+}
